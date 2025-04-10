6a4795627e200d2f0ec4178099013898dea63a02
Mary <miluskasaire512@gmail.com>
2025-04-10 16:56:42 -0500

    cambios antes de vrs 19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 ﻿import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
@@ -16,27 +16,19 @@ import { HomeComponent } from './home';
 // Importa el nuevo módulo de productos
 import { ProductModule } from './product/product.module';
 
-@NgModule({
-    imports: [
-        BrowserModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        AppRoutingModule,
-        
-    ],
-    declarations: [
+@NgModule({ declarations: [
         AppComponent,
         AlertComponent,
         HomeComponent
     ],
-    providers: [
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        ReactiveFormsModule,
+        AppRoutingModule], providers: [
         { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AccountService] },
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-
         // provider used to create fake backend
-        fakeBackendProvider
-    ],
-    bootstrap: [AppComponent]
-})
+        fakeBackendProvider,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
